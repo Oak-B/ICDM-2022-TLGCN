95692bae5a6928d7307e291625e3c483db69e2d8
Oak-B <62462127+Oak-B@users.noreply.github.com>
2022-10-08 01:10:19 +0800

    Delete TLGCN directory

--- TLGCN/utils/globalData.py
+++
@@ -1,7 +0,0 @@
-def init():
-    global a
-    a = {}
-def set(arg,value):
-    a[arg] = value
-def get(arg):
-    return a[arg]
--- TLGCN/utils/jit.py
+++
@@ -1,3 +0,0 @@
-from torch.utils.cpp_extension import load
-
-smo = load("smo", ["TLGCN/utils/smo_cuda.cpp", "TLGCN/utils/smo_cuda_kernel.cu"], verbose=True)
